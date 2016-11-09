901ff80bde5f066c19cc526702645fe43a34d3f7
Juan Jesús Tomás Rojas <juanjetomas@hotmail.com>
2016-11-09 19:01:58 +0100

    Redirección de raiz a app

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
 from registration.backends.simple.views import RegistrationView
+from django.http import HttpResponseRedirect
 
 class MyRegistrationView(RegistrationView):
 	def get_success_url(self,request, user):
@@ -14,6 +15,7 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 	url(r'^rango/', include('rango.urls')),
+	(r'^$', lambda r: HttpResponseRedirect('rango/')),
 	url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
 	url(r'^accounts/', include('registration.backends.simple.urls')),
 )
