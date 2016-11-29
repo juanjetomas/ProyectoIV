10f8ee751ac66cc05068e6d5628c18ed4924d883
Juan Jesús Tomás Rojas <juanjetomas@hotmail.com>
2016-11-29 17:15:17 +0100

    Acceso a la carpeta del proyecto desde la raíz ref #15
    Se cambia a la carpeta desde la raiz para permitir que el script se pueda ejecutar desde cualquier lugar

--- ejecucion_desde_docker.sh
+++ ejecucion_desde_docker.sh
@@ -2,5 +2,5 @@
 #Obtiene la IP de la interfaz eth0 y ejecuta la aplicación en dicha ip con el puerto 8000
 
 IP=$(ifconfig eth0 | grep "inet addr" | cut -d ':' -f 2 | cut -d ' ' -f 1)
-cd ProyectoIV
+cd /ProyectoIV
 python3.5 manage.py runserver $IP:8000
