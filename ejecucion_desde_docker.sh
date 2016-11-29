ed3f44857d49373c9427cc745e2ec7eefc9fb68e
Juan Jesús Tomás Rojas <juanjetomas@hotmail.com>
2016-11-29 12:57:56 +0100

    Corregidos erroes varios en el script de docker ref #15
    - Eliminado el cambio de carpeta, el script está en el directorio del proyecto
    - Asignación correcta de la variable

--- ejecucion_desde_docker.sh
+++ ejecucion_desde_docker.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-#Obtiene la IP de la interfaz eth0 y ejecuta la aplicación en dicha ip con el puerto 8000
+#Obtiene la IP de la interfaz eth0 y ejecuta la aplicaci  n en dicha ip con el puerto 8000
 
-IP=ifconfig eth0 | grep "inet addr" | cut -d ':' -f 2 | cut -d ' ' -f 1
-IP_with_port="${var}:8000"
-cd ProyectoIV
-python3.5 manage.py runserver IP_with_port
+IP=$(ifconfig eth0 | grep "inet addr" | cut -d ':' -f 2 | cut -d ' ' -f 1)
+python3.5 manage.py runserver $IP:8000
