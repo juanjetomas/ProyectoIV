29d31146f879df9e3ecb45e77173f1ba463b73eb
Juan Jesús Tomás Rojas <juanjetomas@hotmail.com>
2016-11-29 20:28:52 +0100

    Añadida la migración y populado antes de la ejecutción ref #15

--- ejecucion_desde_docker.sh
+++ ejecucion_desde_docker.sh
@@ -3,4 +3,6 @@
 
 IP=$(ifconfig eth0 | grep "inet addr" | cut -d ':' -f 2 | cut -d ' ' -f 1)
 cd /ProyectoIV
+python3.5 manage.py migrate
+python3.5 populate.py
 python3.5 manage.py runserver $IP:8000
