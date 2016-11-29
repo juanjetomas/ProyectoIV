58ee02339b4072cf1ac78f61ab7f14290773daa2
Juan Jesús Tomás Rojas <juanjetomas@hotmail.com>
2016-11-29 21:20:08 +0100

    Cambiado start por restart para reiniciar #15

--- ejecucion_desde_docker.sh
+++ ejecucion_desde_docker.sh
@@ -4,7 +4,7 @@
 IP=$(ifconfig eth0 | grep "inet addr" | cut -d ':' -f 2 | cut -d ' ' -f 1)
 cd /ProyectoIV
 service postgresql start
-service postgresql start
+service postgresql restart
 python3.5 manage.py migrate
 python3.5 populate_rango.py
 python3.5 manage.py runserver $IP:8000
