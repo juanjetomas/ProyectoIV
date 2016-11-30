82306fb7ab8985ac4ac0b8cbe0e751910a05ade2
Juan Jesús Tomás Rojas <juanjetomas@hotmail.com>
2016-11-30 17:06:37 +0100

    Añadida comprobación de docker múltiple ref #15
    Se comprueba la variable de entorno de múltiples dockers, y en el caso de usarse
    el contenedor individual que contiene web + DB inicializa postgresql

--- ejecucion_desde_docker.sh
+++ ejecucion_desde_docker.sh
@@ -3,8 +3,12 @@
 
 IP=$(ifconfig eth0 | grep "inet addr" | cut -d ':' -f 2 | cut -d ' ' -f 1)
 cd /ProyectoIV
-service postgresql start
-service postgresql restart
+
+##Si se ejecuta todo en un mismo docker, arranca directamente la BD
+if [ -z ${DOCKERMULTIPLE+x} ] then
+  service postgresql start
+  service postgresql restart
+fi
 python3.5 manage.py migrate
 python3.5 populate_rango.py
 python3.5 manage.py runserver $IP:8000
